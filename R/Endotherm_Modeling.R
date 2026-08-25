cf0d65156c33c9cddf6587a9544e4a833a87e94a
BSpen0820 <96501657+BSpen0820@users.noreply.github.com>
2026-08-25 14:03:57 -0700

    Fix command string formatting for shell execution

--- R/Endotherm_Modeling.R
+++ R/Endotherm_Modeling.R
@@ -261,9 +261,9 @@ run_endotherm_model <- function(workspace_dir, exe_name = "Endo2022a.exe",
 
     # Quote paths so spaces/special characters don't break the shell command
     cmd <- paste0(
-      "printf 'alomvars.dat\\nendo.dat\\n' |",
-      "WINEPREFIX=", shQuote(wineprefix),
-      "wine", shQuote(exe_path)
+      "printf 'alomvars.dat\\nendo.dat\\n' | ",
+      "WINEPREFIX=", shQuote(wineprefix), " ",
+      "wine ", shQuote(exe_path)
     )
 
     wine_output <- system(
